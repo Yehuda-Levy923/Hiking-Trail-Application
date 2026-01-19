17ca3a5495751676f4d9efe913a5c46443c4c1cc
Yehuda-Levy923 <yehuda.levy08@gmail.com>
2026-01-20 01:12:23 +0200

    Change Navbar to NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,24 @@
 import { Routes, Route } from "react-router-dom";
-import { FavoritesProvider } from "./context/FavoritesContext";
-import NavBar from "./components/Navbar";
+
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import Trails from "./pages/Trails";
 import TrailDetailsPage from "./pages/TrailDetailsPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Profile from "./pages/Profile";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
-import Favorites from "./pages/Favorites";
 
 export default function App() {
   return (
-    <FavoritesProvider>
+    <>
       <NavBar />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/trails" element={<Trails />} />
+@@ -17,8 +22,12 @@ export default function App() {
           <Route path="/trails/:id" element={<TrailDetailsPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </FavoritesProvider>
+    </>
   );
 }
